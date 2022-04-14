267d67038c04aca9abd66078399875f8c76d5055
Toh Yan Jie <yanjie20009@gmail.com>
2022-04-14 12:24:04 +0800

    Change settings

--- list/settings.py
+++ list/settings.py
@@ -133,5 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
 ]
